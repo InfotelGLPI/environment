1db96e23daeac386a2de21d6bd4e81b883622a56
Xavier CAILLAUD <tsmr@users.noreply.github.com>
2016-09-24 11:54:39 +0200

    Update headers & XML & Setup

--- front/display.php
+++ front/display.php
@@ -2,30 +2,30 @@
 /*
  * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
- Environment plugin for GLPI
- Copyright (C) 2003-2011 by the Environment Development Team.
+ environmen plugin for GLPI
+ Copyright (C) 2009-2016 by the environmen Development Team.
 
- https://forge.indepnet.net/projects/environment
+ https://github.com/InfotelGLPI/environmen
  -------------------------------------------------------------------------
 
  LICENSE
       
- This file is part of Environment.
+ This file is part of environmen.
 
- Environment is free software; you can redistribute it and/or modify
+ environmen is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- Environment is distributed in the hope that it will be useful,
+ environmen is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Environment. If not, see <http://www.gnu.org/licenses/>.
+ along with environmen. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
-*/
+ */
 
 include ('../../../inc/includes.php');
 
--- setup.php
+++ setup.php
@@ -2,30 +2,30 @@
 /*
  * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
- Environment plugin for GLPI
- Copyright (C) 2003-2011 by the Environment Development Team.
+ environmen plugin for GLPI
+ Copyright (C) 2009-2016 by the environmen Development Team.
 
- https://forge.indepnet.net/projects/environment
+ https://github.com/InfotelGLPI/environmen
  -------------------------------------------------------------------------
 
  LICENSE
       
- This file is part of Environment.
+ This file is part of environmen.
 
- Environment is free software; you can redistribute it and/or modify
+ environmen is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- Environment is distributed in the hope that it will be useful,
+ environmen is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Environment. If not, see <http://www.gnu.org/licenses/>.
+ along with environmen. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
-*/
+ */
 
 // Init the hooks of the plugins -Needed
 function plugin_init_environment() {
@@ -53,7 +53,7 @@ function plugin_version_environment() {
 
    return array (
       'name' => __('Environment', 'environment'),
-      'version' => '1.9.0',
+      'version' => '2.0.0',
       'license' => 'GPLv2+',
       'author'  => "<a href='http://infotel.com/services/expertise-technique/glpi/'>Infotel</a>",
       'homepage'=>'https://github.com/InfotelGLPI/environment',
@@ -64,7 +64,7 @@ function plugin_version_environment() {
 
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_environment_check_prerequisites() {
-   if (version_compare(GLPI_VERSION,'0.90','lt') || version_compare(GLPI_VERSION,'0.91','ge')) {
+   if (version_compare(GLPI_VERSION,'0.90','lt') || version_compare(GLPI_VERSION,'9.2','ge')) {
       _e('This plugin requires GLPI >= 0.90', 'environment');
       return false;
    }
